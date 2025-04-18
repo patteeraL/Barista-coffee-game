d6b3d5225465e3f298613e2fc5994e32689b6db5
Primm <161117867+supaphan@users.noreply.github.com>
2025-04-18 21:44:37 +0700

    prim: create 2 files

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class SoundManager
+public class SoundManager : MonoBehaviour
 {
-    
+    private void Start() {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e){
+        //Playsound
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e){
+        throw new System.NotImplementedException();
+    }
+
+    private void Playsound(AudioClip audioClip, Vector3 position, float volume = 1f){
+        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
 }
