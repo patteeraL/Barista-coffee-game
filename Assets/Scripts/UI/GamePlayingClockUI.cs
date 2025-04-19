2d38d11596f5a7b414a0bc80f893453245d06bcc
Primm <161117867+supaphan@users.noreply.github.com>
2025-04-19 14:35:53 +0700

    Main Menu and Loading finish

--- Assets/Scripts/UI/GamePlayingClockUI.cs
+++ Assets/Scripts/UI/GamePlayingClockUI.cs
@@ -5,5 +5,7 @@ public class GamePlayingClockUI : MonoBehaviour
 {
     [SerializeField] private Image timerImage;
 
-    
+    private void Update(){
+        timerImage.fillAmount = KitchenGameManager.Instance.GetGamePlayingTimerNormalized();
+    }
 }
