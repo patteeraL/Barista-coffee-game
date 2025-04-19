a5185c8bc10e3edcb320a365d4310949e5b16309
Primm <161117867+supaphan@users.noreply.github.com>
2025-04-19 16:52:58 +0700

    option and audio level finish

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
     public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+
     private void Awake(){
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
     private void Start() {
@@ -55,7 +61,20 @@ public class SoundManager : MonoBehaviour {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
     }
 
-    public void PlayFootstepsSound(Vector3 position, float volume){
-        PlaySound(audioClipRefsSO.footstep, position, volume);
+    public void PlayFootstepsSound(Vector3 position, float volumeMultiplier){
+        PlaySound(audioClipRefsSO.footstep, position, volumeMultiplier * volume);
+    }
+
+    public void ChangeVolume(){
+        volume += 0.1f;
+        if (volume > 1f){
+            volume = 0f;
+        }
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume(){
+        return volume;
     }
 }
--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -6,6 +6,7 @@ public class GamePauseUI : MonoBehaviour
 
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
     private void Awake(){
         resumeButton.onClick.AddListener(() => {
@@ -13,6 +14,9 @@ public class GamePauseUI : MonoBehaviour
         });
         mainMenuButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
         });
     }
 
